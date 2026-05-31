b6b5fb358572f7a9d6fbdd4efb9a66d9667e410f
sabuthess <dilan.developer22@gmail.com>
2026-05-31 10:42:41 -0500

    Add barrel icons

---
+++ src/components/ui/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as ShoppingCart } from "./ShoppingCartIcon";
+export { default as SearchIcon } from "./SearchIcon";
+export { default as  PasswordIcon} from "./PasswordIcon";
+export { default as MoonIcon  } from "./MoonIcon";
+export { default as SunIcon } from "./SunIcon";
+export { default as UserIcon } from "./UserIcon";
+export { default as TrashIcon } from "./TrashIcon";
